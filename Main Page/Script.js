5698b02ff2f6a953a1751215540626c471ed240e
Evan B <51836263+Scaledish@users.noreply.github.com>
2020-02-12 16:39:01 -0500

    Add files via upload

---
+++ Main Page/Script.js
@@ -0,0 +1,10 @@
+function openabout() {
+  document.getElementById("about").style.display = "block";
+  console.log("About Launch Atempted")
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeabout() {
+  document.getElementById("about").style.display = "none";
+  console.log("About Close Atempted");
+}
